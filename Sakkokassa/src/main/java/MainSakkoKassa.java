b05a054ccad859b7e98896386eb3ddd6a63408c6
Niklas N <niri.niiranen@gmail.com>
2020-03-31 14:19:47 +0300

    Sakkokassa

--- Sakkokassa/src/main/java/MainSakkoKassa.java
+++ Sakkokassa/src/main/java/MainSakkoKassa.java
@@ -3,13 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.sakkokassa;
 
 /**
  *
  * @author niri91
  */
-public class MainSakkokassa {
+public class MainSakkoKassa {
 
     /**
      * @param args the command line arguments
