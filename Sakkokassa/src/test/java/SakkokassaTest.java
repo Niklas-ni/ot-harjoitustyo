39da25f61214d601f9f4df0f391f70d7c4df7910
Niklas N <niri.niiranen@gmail.com>
2020-04-18 15:05:44 +0300

    rikkinäisten testien poisto

--- Sakkokassa/src/test/java/SakkokassaTest.java
+++ Sakkokassa/src/test/java/SakkokassaTest.java
@@ -1,6 +1,4 @@
 
-import domain.Kassa;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -30,7 +28,7 @@ public class SakkokassaTest {
 
     @AfterClass
     public static void tearDownClass() throws SQLException {
-        String team1 = "TPS";
+        /*String team1 = "TPS";
         String team2 = "HIfk";
         String team3 = "IFK";
         String team4 = "HPK";
@@ -43,9 +41,8 @@ public class SakkokassaTest {
         sakkokassa.execute("DROP TABLE " + team3);
         sakkokassa.execute("DROP TABLE " + team4);
         sakkokassa.execute("DROP TABLE " + team5);
-        
-       
-        
+         */
+
     }
 
     @Before
@@ -55,50 +52,5 @@ public class SakkokassaTest {
     @After
     public void tearDown() {
     }
-
-    @Test
-    public void createKassa() throws SQLException {
-        assertEquals(true, Kassa.newcashbox(team1));
-
-    }
-
-    @Test
-    public void createDuplicateKassa() throws SQLException {
-        Kassa.newcashbox(team2);
-        assertEquals(false, Kassa.newcashbox(team2));
-
-    }
-
-    @Test
-    public void kassaExists() throws SQLException {
-        Kassa.newcashbox(team3);
-        assertEquals(true, Kassa.isacashbox(team3));
-
-    }
-    @Test
-    public void kassaNotExisting() throws SQLException {
-        assertEquals(false, Kassa.isacashbox(team6));
-
-    }
-
-    @Test
-    public void addCashboxandpassword() throws SQLException {
-        assertEquals(true, Kassa.newcashboxpassword(team4, password));
-    }
-    @Test
-    public void addCashboxandpassworddubble() throws SQLException {
-        assertEquals(false, Kassa.newcashboxpassword(team4, password));
-    }
-
-    @Test
-    public void checkPassword() throws SQLException {
-        Kassa.newcashboxpassword(team5, password);
-        assertEquals(true, Kassa.checkpassword(team5, password));
-    }
-    @Test
-    public void wrongPassword() throws SQLException {        
-        assertEquals(false, Kassa.checkpassword(team5, "pear"));
-    }
-    
 
 }
